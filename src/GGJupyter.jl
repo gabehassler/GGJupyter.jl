e05d63e607fc884f181a96f74f21b0b78de22f85
Gabe Hassler <ghassler@rand.org>
2023-01-11 16:21:23 -0800

    rlibrary is super cool

--- src/GGJupyter.jl
+++ src/GGJupyter.jl
@@ -3,6 +3,9 @@ module GGJupyter
 export gg_jupyter
 
 using RCall
+@rlibrary ggplot2
+
+using DataFrames
 
 const GG_ENV = Dict{String, Any}(
     "height" => 7,
@@ -10,24 +13,15 @@ const GG_ENV = Dict{String, Any}(
 )
 
 function gg_jupyter(;kw_args...)
-    gg_jupyter(""; kw_args...)
+    gg_jupyter(last_plot(); kw_args...)
 end
 
 function gg_jupyter(plt;
                     height::Real = GG_ENV["height"],
                     width::Real = GG_ENV["width"])
-    path = tempname() * ".svg"
-    @rput plt
-    @rput path
-    @rput height
-    @rput width
 
-    R"""
-    if (plt == "") {
-        plt = last_plot()
-    }
+    path = tempname() * ".svg"
     ggsave(path, plot = plt, height = height, width = width)
-    """
     open(path) do f
         display("image/svg+xml", read(f, String))
     end
