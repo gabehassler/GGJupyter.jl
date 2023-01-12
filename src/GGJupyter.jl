48a6b3bdf1150cbdeff4ef56ce760ff6e805981b
Gabe Hassler <ghassler@rand.org>
2023-01-11 17:12:42 -0800

    no more rlibrary :(

--- src/GGJupyter.jl
+++ src/GGJupyter.jl
@@ -1,9 +1,19 @@
 module GGJupyter
 
-export gg_jupyter
+export gg_jupyter,
+       quick_hist
 
 using RCall
-@rlibrary ggplot2
+
+function setup_R()
+    R"""
+    library(ggplot2)
+    themes <- list(
+        default = theme_minimal() +
+            theme(plot.title = element_text(hjust = 0.5))
+    )
+    """
+end
 
 using DataFrames
 
@@ -13,19 +23,48 @@ const GG_ENV = Dict{String, Any}(
 )
 
 function gg_jupyter(;kw_args...)
-    gg_jupyter(last_plot(); kw_args...)
+    gg_jupyter(""; kw_args...)
 end
+
+
 
 function gg_jupyter(plt;
                     height::Real = GG_ENV["height"],
                     width::Real = GG_ENV["width"])
 
+    @rput plt
+    @rput height
+    @rput width
+
     path = tempname() * ".svg"
+    @rput path
+    R"""
+    if (plt == "") {
+        plt = last_plot()
+    }
     ggsave(path, plot = plt, height = height, width = width)
+    """
     open(path) do f
         display("image/svg+xml", read(f, String))
     end
     nothing
+end
+
+function quick_hist(df::DataFrame, xvar::String;
+                    title::String = "",
+                    theme::String = "default")
+    @rput df
+    @rput xvar
+    @rput title
+    this_theme = theme
+    @rput this_theme
+    R"""
+    ggplot(df, aes(x = df[[xvar]])) + geom_histogram() +
+        ggtitle(title) +
+        xlab(xvar) +
+        themes[this_theme]
+    """
+
 end
 
 end # module GGJupyter
