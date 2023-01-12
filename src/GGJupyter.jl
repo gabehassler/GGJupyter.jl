cec43c913c1fcb1a268843f6c421915d347bd24b
Gabe Hassler <ghassler@rand.org>
2023-01-11 16:02:03 -0800

    somewhat hacky way to deal with last_plot() issue

--- src/GGJupyter.jl
+++ src/GGJupyter.jl
@@ -1,11 +1,17 @@
 module GGJupyter
 
+export gg_jupyter
+
 using RCall
 
 const GG_ENV = Dict{String, Any}(
     "height" => 7,
     "width" => 7
 )
+
+function gg_jupyter(;kw_args...)
+    gg_jupyter(""; kw_args...)
+end
 
 function gg_jupyter(plt;
                     height::Real = GG_ENV["height"],
@@ -17,6 +23,9 @@ function gg_jupyter(plt;
     @rput width
 
     R"""
+    if (plt == "") {
+        plt = last_plot()
+    }
     ggsave(path, plot = plt, height = height, width = width)
     """
     open(path) do f
